2739062a143598a32c0bdd8cb3f7f6f6c1f35e8e
luisfuentech <luisfuentes.au@gmail.com>
2020-01-05 10:26:20 -0500

    perf: add car game

---
+++ python-mosh/car_game.py
@@ -0,0 +1,28 @@
+command = ""
+started = False
+
+while True:
+    command = input(">").lower()
+
+    if command == "help":
+        print("""
+start - to start the car
+stop - to stop de cart
+quit - to exit
+        """)
+    elif command == "start":
+        if started:
+            print("Car already started")
+        else:
+            started = True
+            print("Car started... Ready to go!")
+    elif command == "stop":
+        if not started:
+            print("Car already stopped.")
+        else:
+            started = False
+            print("Car stopped")
+    elif command == "quit":
+        break
+    else:
+        print("Sorry, I don't understand that...")
